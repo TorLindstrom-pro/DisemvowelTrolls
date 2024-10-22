487220d2accd18ad0b5d031011cae2dfa6945179
TorLindstrom <tor.lindstrom@winlas.se>
2024-10-22 16:59:14 +0200

    feat: remove vowels from string

--- DisemvowelTrolls/Kata.cs
+++ DisemvowelTrolls/Kata.cs
@@ -4,6 +4,9 @@ public class Kata
 {
 	public static string Disemvowel(string str)
 	{
-		return str;
+		var vowels = new[]{'a', 'e', 'i', 'o', 'u'};
+		return str
+			.Where(c => !vowels.Contains(c))
+			.Aggregate("", (result, c) => result + c);
 	}
 }
--- NTest/UnitTest1.cs
+++ NTest/UnitTest1.cs
@@ -10,4 +10,18 @@ public class Tests
         var result = Kata.Disemvowel("qwrt");
         Assert.That(result, Is.EqualTo("qwrt"));
     }
+    
+    [Test]
+    public void InputIsOnlyVowels_ReturnsEmptyString()
+    {
+        var result = Kata.Disemvowel("aeiou");
+        Assert.That(result, Is.EqualTo(""));
+    }
+    
+    [Test]
+    public void VowelsInInput_ReturnsInputWithoutVowels()
+    {
+        var result = Kata.Disemvowel("hello");
+        Assert.That(result, Is.EqualTo("hll"));
+    }
 }
